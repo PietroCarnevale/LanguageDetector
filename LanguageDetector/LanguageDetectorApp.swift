0e22fd955c308938ed99346f368498a6ee953932
Pietro <pietro.carnevale.01@gmail.com>
2023-11-23 15:26:43 +0100

    Initial Commit

---
+++ LanguageDetector/LanguageDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageDetectorApp.swift
+//  LanguageDetector
+//
+//  Created by Pietro Carnevale on 22/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LanguageDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ LanguageDetector/MainView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  LanguageDetector
+//
+//  Created by Pietro Carnevale on 16/11/23.
+//
+
+import SwiftUI
+import CoreML
+
+let mlModel = LanguageDetectorModel()
+
+
+struct MainView: View {
+    
+    @State private var sentence: String = ""
+    @State private var answer: String = "Here you will see the answer!"
+    @State private var pulseAnimation: Bool = false
+    
+    var body: some View {
+        NavigationStack{
+            Image(systemName: "globe")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(80)
+                .foregroundStyle(.blue)
+                .symbolEffect(.pulse, value: pulseAnimation)
+                .accessibilityHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+            HStack {
+                TextField("Write here...", text: $sentence)
+                    .padding()
+                    .textFieldStyle(.roundedBorder)
+                Button(action: {
+                    sentence = ""
+                    answer = "Here you will see the answer!"
+                }, label: {
+                    Text("CLEAR")
+                })
+                Spacer()
+            }
+            Button(action: {
+                let expectedValue = try? mlModel.prediction(text: sentence)
+                pulseAnimation.toggle()
+                answer = expectedValue!.label
+            }, label: {
+                Text("DETECT")
+                    .bold()
+            })
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            Text(answer)
+                .font(.title3)
+                .padding()
+            Spacer()
+            .navigationTitle("Language Detector")
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
